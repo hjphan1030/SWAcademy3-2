4c6950579319370dfd70e2437dce06a16fd6c097
한현준 <hjphan1030@gmail.com>
2023-08-23 15:10:21 +0900

    Add files via upload
    Initializing project.

---
+++ sw2023/src/main/java/com/cnu/sw2023/Sw2023Application.java
@@ -0,0 +1,13 @@
+package com.cnu.sw2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sw2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sw2023Application.class, args);
+	}
+
+}
