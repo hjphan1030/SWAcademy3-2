4597f74e94ea3a79325d3da317dd9929633f2f40
seongmuk Lim <tjdanr@gmail.com>
2023-08-30 15:04:40 +0900

    최신순 슬라이드 코드 추가

--- src/main/java/com/cnu/sw2023/index/controller/IndexController.java
+++ src/main/java/com/cnu/sw2023/index/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.cnu.sw2023.index.controller;
 import com.cnu.sw2023.index.dto.MainPostDto;
 import com.cnu.sw2023.like.domain.PostLike;
 import com.cnu.sw2023.post.service.PostService;
+import com.cnu.sw2023.restaurant.dto.RestaurantDTO;
 import org.springframework.http.ResponseEntity;
 import com.cnu.sw2023.index.service.IndexService;
 import com.cnu.sw2023.post.domain.Post;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.HashMap;
@@ -26,27 +28,19 @@ import java.util.stream.Stream;
 @RequestMapping("/main")
 public class IndexController {
     private final IndexService indexService;
-    @GetMapping("/slide")
-    public ResponseEntity<Map<String, Object>> getSlide(){
-        Map<String, Object> response = new HashMap<>();
-        Map<Long, String> res = new HashMap<>();
-        Stream<String> titleStream = indexService
-                    .findPostLikesSortedByLikesDescending().stream().map(postLike -> postLike.getPost().getTitle());
-        Stream<Long> postIdStream = indexService
-                .findPostLikesSortedByLikesDescending().stream().map(postLike -> postLike.getPost().getId());
-        List<String> titleList = titleStream.collect(Collectors.toList());
-        List<Long> postIdList = postIdStream.collect(Collectors.toList());
-        for(int i = 0; i < titleList.size(); i++){
-            res.put(postIdList.get(i), titleList.get(i));
-        }
-        response.put("slide", res);
-        return ResponseEntity.ok().body(response);
-    }
 
     @GetMapping("/popular")
     public ResponseEntity<List<MainPostDto>> getPopularPosts() {
         List<MainPostDto> popularPosts = indexService.getPopularPosts();
         return ResponseEntity.ok(popularPosts);
     }
-
+    @GetMapping("/slide/recent")
+    public HashMap<String, String> getRecentRestaurant(){
+        ArrayList<String> restaurantsNames = indexService.getRecentRestaurant();
+        HashMap<String, String> map = new HashMap<>();
+        for(String name : restaurantsNames){
+            map.put(name, "boards/" + name +"/");
+        }
+        return map;
+    }
 }
--- src/main/java/com/cnu/sw2023/index/service/IndexService.java
+++ src/main/java/com/cnu/sw2023/index/service/IndexService.java
@@ -5,11 +5,14 @@ import com.cnu.sw2023.like.repository.PostLikeRepository;
 import com.cnu.sw2023.post.domain.Post;
 import com.cnu.sw2023.post.repository.PostRepository;
 import com.cnu.sw2023.restaurant.domain.Restaurant;
+import com.cnu.sw2023.restaurant.dto.RestaurantDTO;
 import com.cnu.sw2023.restaurant.repository.RestaurantRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,6 +20,7 @@ import java.util.stream.Stream;
 @Service
 @AllArgsConstructor
 public class IndexService {
+    private final RestaurantRepository restaurantRepository;
     private final PostRepository postRepository;
     private final PostLikeRepository postLikeRepository;
     public List<PostLike> findPostLikesSortedByLikesDescending() {
@@ -30,5 +34,14 @@ public class IndexService {
         return popularPosts.stream()
                 .map(post -> new MainPostDto(post.getId(), post.getTitle(), post.getLikeCount(), post.getCreatedAt()))
                 .collect(Collectors.toList());
+    }
+
+    public ArrayList<String> getRecentRestaurant() {
+        List<Restaurant> restaurantsList = restaurantRepository.findTop3ByOrderByIdDesc();
+        ArrayList<String> restaurantNames = new ArrayList<>();
+        for(Restaurant r : restaurantsList){
+            restaurantNames.add(r.getRestaurantName());
+        }
+        return restaurantNames;
     }
 }
