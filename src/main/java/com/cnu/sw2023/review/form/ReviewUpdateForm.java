860dddd200b2ca1a3e9c338dd2bac5cc10f89df2
Hyunji Jin <jinhyunji520@gmail.com>
2023-09-07 15:52:47 +0900

    리뷰 API 완료

---
+++ src/main/java/com/cnu/sw2023/review/form/ReviewUpdateForm.java
@@ -0,0 +1,10 @@
+package com.cnu.sw2023.review.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ReviewUpdateForm {
+    private String content;
+    private int rating;
+}
