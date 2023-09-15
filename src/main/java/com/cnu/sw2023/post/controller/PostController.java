c8f7e9b9de9a678dda2c2db4f001957026567b66
tangba484 <tangba484@naver.com>
2023-09-15 17:56:37 +0900

    It

--- src/main/java/com/cnu/sw2023/post/controller/PostController.java
+++ src/main/java/com/cnu/sw2023/post/controller/PostController.java
@@ -154,17 +154,6 @@ public class PostController {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
         }
     }
-    @PostMapping("/{postId}/update")
-    public ResponseEntity<Map<String,String>> updatePost(@PathVariable Long postId, @RequestBody PostUpdateForm postUpdateForm){
-        String content = postUpdateForm.getContent();
-        postService.updatePost(postId,content);
-        Map<String, String> res = new HashMap<>();
-        res.put("message","수정 완료");
-        return ResponseEntity.ok().body(res);
-    }
-}
-
-
 //    @PostMapping("/{postId}/update")
 //    public ResponseEntity<Map<String,String>> updatePost(@PathVariable Long postId, @RequestBody PostUpdateForm postUpdateForm){
 //        String content = postUpdateForm.getContent();
@@ -173,4 +162,4 @@ public class PostController {
 //        res.put("message","수정 완료");
 //        return ResponseEntity.ok().body(res);
 //    }
-//}
+}
