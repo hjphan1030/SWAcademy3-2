575eb29ba9f32af616ad614ca8d0f38f7bd2d3cb
Hyunji Jin <jinhyunji520@gmail.com>
2023-09-15 17:55:08 +0900

    게시글 관련 API 완성

--- src/main/java/com/cnu/sw2023/like/repository/CommentLikeRepository.java
+++ src/main/java/com/cnu/sw2023/like/repository/CommentLikeRepository.java
@@ -10,6 +10,4 @@ import java.util.Optional;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
     Optional<CommentLike> findByCommentIdAndEmail (Long commentId,String email);
-
-
 }
